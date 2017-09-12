138e826c317b0924e3a34e07fdd050c0740cab21
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-11 22:03:51 -0400

    added form validation

--- javaScriptQuiz/quiz.js
+++ javaScriptQuiz/quiz.js
@@ -10,5 +10,16 @@ function submitAnswers(){
 	var q4 = document.forms["quizForm"]["q4"].value;
 	var q5 = document.forms["quizForm"]["q5"].value;
 
-	alert(q1);
+	// Validation
+	
+
+	for (i = 1; i <= total; i++) {
+		if (eval('q'+i) == null || eval('q'+i) == "") {
+			alert('You missed question '+ i);
+			return false;
+		}
+	}
+
+	return false;
+
 }
